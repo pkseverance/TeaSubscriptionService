0a95cef2454b28cdd2ea186a918aa07ecfae69a6
Patricia Severance <patriciakseverance@gmail.com>
2023-04-20 16:32:31 -0600

    Add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Customer.create!(first_name: 'John', last_name: 'Smith', email: 'johnsmith@example.com', address: '1234 Smith Dr.')
+Subscription.create!(title: 'Jade Dragon Experience', price: 19.99, frequency: 'monthly')

9187b0d763146821a82c319bcc3867f6a7dff0ed
Patricia Severance <patriciakseverance@gmail.com>
2023-04-20 16:31:51 -0600

    Implement customer subscription create endpoint

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
-
+  def create
+    customer = Customer.find(params[:customer_id])
+    customer_subscription = customer.customer_subscriptions.create!(subscription_id: params[:subscription_id], status: true)
+    render json: CustomerSubscriptionSerializer.new(customer_subscription), status: 201
+  end
 end

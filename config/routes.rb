8e2cde9ec6915be5daa1b9a556d6eae61dc15851
Patricia Severance <patriciakseverance@gmail.com>
2023-04-20 18:15:23 -0600

    Add customer subscriptions GET endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customer_subscriptions, only: [:create]
+      resources :customers, only: [] do
+        resources :subscriptions, only: [:index]
+      end
     end
   end
 end

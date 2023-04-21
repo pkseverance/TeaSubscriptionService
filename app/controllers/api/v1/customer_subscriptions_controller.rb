a7da589d48743b2341dd54a6d5a33c87203b8717
Patricia Severance <patriciakseverance@gmail.com>
2023-04-20 19:00:33 -0600

    Add update method to customer_subscriptions_controller

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     customer_subscription = customer.customer_subscriptions.create!(subscription_id: params[:subscription_id], status: true)
     render json: CustomerSubscriptionSerializer.new(customer_subscription), status: 201
   end
+
+  def update
+    customer_subscription = CustomerSubscription.find(params[:id])
+    customer_subscription.update(status: params[:status])
+    render json: CustomerSubscriptionSerializer.new(customer_subscription)
+  end
 end

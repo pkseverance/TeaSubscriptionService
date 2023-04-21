4bcf7918a240b784c1e8d7278cfdb2301ab12c93
Patricia Severance <patriciakseverance@gmail.com>
2023-04-20 18:59:51 -0600

    Add update to customer_subscriptions endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customer_subscriptions, only: [:create]
+      resources :customer_subscriptions, only: [:create, :update]
       resources :customers, only: [] do
         resources :subscriptions, only: [:index]
       end
